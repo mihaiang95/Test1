cc8d9ddfee0b0edc7be7e2b72be47891163bf8c9
Mihai <anghelmihaialexandru@gmail.com>
2019-07-22 20:00:16 +0300

    First commit

--- WebApplication3/App_Start/RouteConfig.cs
+++ WebApplication3/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace WebApplication3
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               name: "Default",
+                            url: "{*anything}",
+                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
